4c4114d787f74b87a77e18ae2e3aa455b5bf5ddd
samxytesla <samuelpatrick24@gmail.com>
2023-02-22 14:24:34 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints 10 times the alphabet, in lowercase.
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
+
