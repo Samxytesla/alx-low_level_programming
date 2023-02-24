11aa61abeb71a58d59cb62374cf4c771ad30724d
samxytesla <samuelpatrick24@gmail.com>
2023-02-24 03:53:16 -0800

    initial

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+nclude "main.h"
+/**
+ * main - Entry level
+ *
+ * Description: 'program to save printf'
+ * 
+ * Result: Always 0 (success)
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c ,1));
+}
