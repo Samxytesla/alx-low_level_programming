931c7bd315d8c5bfbfeedfa92646dbbb80ab05c9
samxytesla <samuelpatrick24@gmail.com>
2023-03-21 14:58:14 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,47 +1,52 @@
-#include "main.h"
+#include <unistd.h>
 #include <stdlib.h>
 /**
- * _strlen - calculate and return string length
- * @string: string
- * Return: string length
- */
-int _strlen(char *string)
-{
-	int i;
-
-	for (i = 0; string[i] != '\0'; i++)
-		;
-	return (i);
-}
-/**
- * string_nconcat - concatenate s1 and n bytes of s2
- * @s1: string 1
- * @s2: string 2
- * @n: n bytes
- * Return: pointer
+ * string_nconcat - Main entry
+ *@s1: pointer
+ *@s2: pointer
+ *@n: unsigned int
+ *Description:function that concatenates two strings
+ * Return:char pointer
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	int num, len, i, j;
+	unsigned int c1, c2, c3, c4;
+	char *pconcat;
 
-	num = n;
+/*c1 y c2 are lenght of s1 and s2*/
+
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	if (num < 0)
+
+	for (c1 = 0; s1[c1] != '\0'; c1++)
+		;
+
+	for (c2 = 0; s2[c2] != '\0'; c2++)
+		;
+
+	if (n >= c2)
+		n = c2;
+
+	pconcat = malloc(sizeof(char) * (c1 + n) + 1);
+
+	if (pconcat != NULL)
+	{
+/* c3 counter acumulated, c4 para recorrer s1 desde cero*/
+		for (c3 = 0; s1[c3] != '\0'; c3++)
+			pconcat[c3] = s1[c3];
+
+		for (c4 = 0; c4 < n; c4++, c3++)
+		pconcat[c3] = s2[c4];
+
+		pconcat[c3] = '\0';
+
+		return (pconcat);
+	}
+	else
+	{
 		return (NULL);
-	if (num >= _strlen(s2))
-		num = _strlen(s2);
-		len = _strlen(s1) + num + 1;
-		p = malloc(sizeof(*p) * len);
-	if (p == NULL)
-		return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
-		p[i] = s1[i];
-	for (j = 0; j < num; j++)
-		p[i + j] = s2[j];
-	p[i + j] = '\0';
-	return (p);
+	}
 }
