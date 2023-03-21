44316a9db6162270db0f476166ffcc02953d5de8
samxytesla <samuelpatrick24@gmail.com>
2023-03-21 10:54:11 -0700

    file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,12 +3,12 @@
 /**
  * main - program that prints the name of the file it was compiled from
  *
- * Return: always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("Name of source file: %s\n",  __FILE__);
+	printf("%s\n",  __FILE__);
 
 	return (0);
 }
