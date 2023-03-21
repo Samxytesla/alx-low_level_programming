d5520e0a40d2a440f1c49f16d5b1f3d214bf095f
samxytesla <samuelpatrick24@gmail.com>
2023-03-21 10:45:51 -0700

    a header file that defines a macro named SIZE

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,12 +1,6 @@
 #ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_h
+#define OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
 #endif
-
-/*
- * main - defines a macro named SIZE
- *
- * Return: always 0
- */
