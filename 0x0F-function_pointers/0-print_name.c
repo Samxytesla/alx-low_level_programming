2e9045e257976baea8f9623b257ae8af0bb28b59
samxytesla <samuelpatrick24@gmail.com>
2023-03-24 11:44:48 -0700

    function pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * print name - A function that prints a name
+ * print_name - A function that prints a name
  * @name: string to add
  * @f: pointer to function
  * Return: nothing
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,12 +9,10 @@
  * Return: void
  */
 
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	 int i;
+	size_t i = 0;
 
-	if (array == NULL || action == NULL)
-		return;
 	for (i = 0 ; i < size ; i++)
 	{
 		action(array[i]);
