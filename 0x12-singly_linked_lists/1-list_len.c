4b45f425afa1ccaa2ff0d1d74d25ed04b05e4d27
samxytesla <samuelpatrick24@gmail.com>
2023-03-30 14:56:56 -0700

    list len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - Calculate the number of elements.
+ * @h: Pointer to a list.
+ * Return: Integer.
+ **/
+
+size_t list_len(const list_t *h)
+{
+	unsigned int nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
